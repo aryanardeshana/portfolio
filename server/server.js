59338023d5196082071729793f5c5e0be816a913
aryanardeshana <aryanpatel5423@gmail.com>
2026-07-23 17:12:51 +0530

    Add SMTP verify

--- server/server.js
+++ server/server.js
@@ -18,6 +18,14 @@ const transporter = nodemailer.createTransport({
     },
 });
 
+transporter.verify((error, success) => {
+    if (error) {
+        console.error("SMTP Error:", error);
+    } else {
+        console.log("SMTP Server is ready");
+    }
+});
+
 app.post("/api/contact", async (req, res) => {
     try {
         const { name, email, subject, message } = req.body;
